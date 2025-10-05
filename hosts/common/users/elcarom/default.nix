9ca93f4401e95816ddf5cb433047f74ff70abdee
Nandonirv <matthewknowles@live.fr>
2025-10-05 21:03:13 +0200

    Separate users from global configuration

---
+++ hosts/common/users/elcarom/default.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, inputs ... }: {
+
+  users.users.elcarom = {
+    isNormalUser = true;
+    initialPassword = "password";
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    packages = [ inputs.home-manager.packages.${pkgs.system}.default ];
+  };
+
+  home-manager.users.elcarom = import ../../../../home/elcarom/${config.networking.hostName}.nix;
+
+}
+
--- hosts/vsvr-nos052/configuration.nix
+++ hosts/vsvr-nos052/configuration.nix
@@ -31,21 +31,6 @@
 
   services.xserver.xkb.layout = "fr";
 
-  users.users.elcarom = {
-    isNormalUser = true;
-    initialPassword = "password";
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    packages = [ inputs.home-manager.packages.${pkgs.system}.default ];
-  };
-
-  home-manager = {
-	  useUserPackages = true;
-	  extraSpecialArgs = { inherit inputs outputs; };
-	  users.elcarom =
-	    import ../../home/elcarom/${config.networking.hostName}.nix;
-	};
-
-
   environment.systemPackages = with pkgs; [
     wget
     git
