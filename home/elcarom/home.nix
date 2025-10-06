50cbd59f257a7667845f97bff380a61f99c3011a
Nandonirv <matthewknowles@live.fr>
2025-10-06 20:45:20 +0200

    Update home.nix

--- home/elcarom/home.nix
+++ home/elcarom/home.nix
@@ -21,23 +21,17 @@
   programs.home-manager.enable = true;
   
   programs.caelestia = {
-  enable = true;
-  systemd = {
-    enable = false; # if you prefer starting from your compositor
-    target = "graphical-session.target";
-    environment = [];
-  };
-  settings = {
-    bar.status = {
-      showBattery = false;
+    enable = true;
+    systemd = {
+      enable = false; # if you prefer starting from your compositor
+      target = "graphical-session.target";
+      environment = [];
     };
-    paths.wallpaperDir = "~/Images";
-  };
-  cli = {
-    enable = true; # Also add caelestia-cli to path
     settings = {
-      theme.enableGtk = false;
+      bar.status = {
+        showBattery = false;
+      };
+      paths.wallpaperDir = "~/Images";
     };
   };
-};
 }
