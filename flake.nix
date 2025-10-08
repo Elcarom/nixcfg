a843c894153ea0b312921acab564077518159b38
Nandonirv <matthewknowles@live.fr>
2025-10-08 22:07:35 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,64 +3,39 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
-
     home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    hyprland.url = "github:hyprwm/Hyprland";
-    hyprland.inputs.nixpkgs.follows = "nixpkgs";
-
-    hyprland-plugins.url = "github:hyprwm/hyprland-plugins";
-    hyprland-plugins.inputs.hyprland.follows = "hyprland";
-
-    quickshell.url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
-    quickshell.inputs.nixpkgs.follows = "nixpkgs";
-
-    systems.url = "github:nix-systems/default-linux";
-    systems.inputs.nixpkgs.follows = "nixpkgs";
-
-    nur.url = "github:nix-community/NUR";
-    nur.inputs.nixpkgs.follows = "nixpkgs";
-
     illogical-impulse-dotfiles.url = "github:end-4/dots-hyprland";
     illogical-impulse-dotfiles.flake = false;
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs:
-    let
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "i686-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
+  let
+    systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" ];
+  in
+  {
+    packages = nixpkgs.lib.genAttrs systems (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+        import ./pkgs { inherit pkgs inputs system; }
+    );
 
-      forAllSystems = nixpkgs.lib.genAttrs systems;
-    in {
-      packages = forAllSystems (system:
-        import ./pkgs { inherit system pkgs inputs; }
-      );
+    homeManagerModules = import ./modules/home-manager;
 
-      overlays = import ./overlays { inherit inputs; };
-
-      homeManagerModules = import ./modules/home-manager;
-
-      nixosConfigurations = {
-        vsvr-nos052 = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          specialArgs = { inherit inputs self; };
-          modules = [ ./hosts/vsvr-nos052.nix ];
-        };
-      };
-
-      homeConfigurations = {
-        "elcarom@vsvr-nos052" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages."x86_64-linux";
-          extraSpecialArgs = { inherit inputs self; };
-          modules = [ ./home/elcarom/vsvr-nos052.nix ];
-        };
+    nixosConfigurations = {
+      vsvr-nos052 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs self; };
+        modules = [ ./hosts/vsvr-nos052.nix ];
       };
     };
+
+    homeConfigurations = {
+      "elcarom@vsvr-nos052" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages."x86_64-linux";
+        extraSpecialArgs = { inherit inputs self; };
+        modules = [ ./home/elcarom/vsvr-nos052.nix ];
+      };
+    };
+  };
 }
