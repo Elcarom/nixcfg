160188149c11caabb6497848931380d5cd1e1d2a
Nandonirv <matthewknowles@live.fr>
2025-10-12 18:19:50 +0200

    Update home.nix

--- home/elcarom/home.nix
+++ home/elcarom/home.nix
@@ -20,4 +20,22 @@
   };
 
   programs.home-manager.enable = true;
+  inputs.illogical-impulse = {
+    enable = true;
+
+    hyprland = {
+        # Use customized Hyprland build
+        package = hypr.hyprland;
+        xdgPortalPackage = hypr.xdg-desktop-portal-hyprland;
+
+        # Enable Wayland ozone
+        ozoneWayland.enable = true;
+    };
+
+    # Dotfiles configurations
+    dotfiles = {
+        fish.enable = true;
+        kitty.enable = true;
+    };
+};
 }
