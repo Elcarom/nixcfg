1e7fad47917787e83ec40a7375bd7d8ae092ce2e
Nandonirv <matthewknowles@live.fr>
2025-10-20 21:48:24 +0200

    feat: add locale configuration and update imports in default.nix

--- hosts/vsvr-nos052/configuration.nix
+++ hosts/vsvr-nos052/configuration.nix
@@ -8,24 +8,8 @@
 
   networking.hostName = "vsvr-nos052";
 
-  time.timeZone = "Europe/Paris";
-
-  i18n.defaultLocale = "en_US.UTF-8";
-
- 	i18n.extraLocaleSettings = {
-	  LC_ADDRESS = "fr_FR.UTF-8";
-	  LC_IDENTIFICATION = "fr_FR.UTF-8";
-	  LC_MEASUREMENT = "fr_FR.UTF-8";
-	  LC_MONETARY = "fr_FR.UTF-8";
-	  LC_NAME = "fr_FR.UTF-8";
-	  LC_NUMERIC = "fr_FR.UTF-8";
-	  LC_PAPER = "fr_FR.UTF-8";
-	  LC_TELEPHONE = "fr_FR.UTF-8";
-	  LC_TIME = "fr_FR.UTF-8";
-	  };
-
   console = {
-    useXkbConfig = true; # use xkb.options in tty.
+    useXkbConfig = true;
   };
 
   services.xserver.xkb.layout = "fr";
@@ -35,14 +19,9 @@
     git
   ];
   
-  programs.hyprland = {
-	    enable = true;
-	    xwayland.enable = true;
-	};
-  
   services.openssh.enable = true;
   
-  hardware.opengl.enable = true;
+  hardware.graphics.enable = true;
   
   system.stateVersion = "25.05";
 
