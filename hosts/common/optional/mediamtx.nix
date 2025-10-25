82f49763dfb317a80e83634b2d4d88ce30fb15e6
Elcarom <elcarom@proton.me>
2025-10-25 16:27:40 +0200

    Update mediamtx.nix

--- hosts/common/optional/mediamtx.nix
+++ hosts/common/optional/mediamtx.nix
@@ -12,7 +12,7 @@
     settings = {
       paths = {
         cam = {
-          runOnInit = "${lib.getExe pkgs.ffmpeg} -y -hwaccel cuda -hwaccel_output_format cuda -f v4l2 -input_format mjpeg -video_size 1920x1080 -framerate 30 -i /dev/video0 -vf scale_cuda=1920:1080 -c:v h264_nvenc -b:v 5M -f rtsp rtsp://localhost:$RTSP_PORT/main -vf scale_cuda=640:360 -c:v h264_nvenc -b:v 8M -f rtsp rtsp://localhost:$RTSP_PORT/sub";
+          runOnInit = "${lib.getExe pkgs.ffmpeg} -y -hwaccel cuda -hwaccel_output_format cuda -f v4l2 -input_format mjpeg -video_size 1920x1080 -framerate 30 -i /dev/video0 -vf scale_cuda=1920:1080, format=yuv420p -c:v h264_nvenc -b:v 5M -pix_fmt yuv420p -f rtsp rtsp://localhost:$RTSP_PORT/main -vf scale_cuda=640:360,format=yuv420p -c:v h264_nvenc -b:v 1M -pix_fmt yuv420p -f rtsp rtsp://localhost:$RTSP_PORT/sub";
           runOnInitRestart = true;
         };
         all_others = {
