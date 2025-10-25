6234464b7aa49590ac16998f8cfbee85dba862bd
Elcarom <elcarom@proton.me>
2025-10-25 22:00:22 +0200

    Simplify cam initialization commands in mediamtx.nix

--- hosts/common/optional/mediamtx.nix
+++ hosts/common/optional/mediamtx.nix
@@ -29,19 +29,8 @@
         };
         cam = {
           runOnInit = ''
-            ${lib.getExe pkgs.ffmpeg-full} -y \
-              -hwaccel cuda -hwaccel_output_format cuda \
-              -f v4l2 -input_format mjpeg -video_size 1920x1080 -framerate 30 -i /dev/video0 \
-              -vf "scale_cuda=1920:1080" \
-              -c:v h264_nvenc -b:v 5M \
-              -f v4l2 /dev/v4l/by-id/video-cam_main &
-
-            ${lib.getExe pkgs.ffmpeg-full} -y \
-              -hwaccel cuda -hwaccel_output_format cuda \
-              -f v4l2 -input_format mjpeg -video_size 1920x1080 -framerate 30 -i /dev/video0 \
-              -vf "scale_cuda=640:360" \
-              -c:v h264_nvenc -b:v 1M \
-              -f v4l2 /dev/v4l/by-id/video-cam_sub &
+            ${lib.getExe pkgs.ffmpeg-full} -y -hwaccel cuda -hwaccel_output_format cuda -f v4l2 -input_format mjpeg -video_size 1920x1080 -framerate 30 -i /dev/video0 -vf "scale_cuda=1920:1080" -c:v h264_nvenc -b:v 5M -f v4l2 /dev/v4l/by-id/video-cam_main
+            ${lib.getExe pkgs.ffmpeg-full} -y -hwaccel cuda -hwaccel_output_format cuda -f v4l2 -input_format mjpeg -video_size 1920x1080 -framerate 30 -i /dev/video0 -vf "scale_cuda=640:360" -c:v h264_nvenc -b:v 1M -f v4l2 /dev/v4l/by-id/video-cam_sub 
           '';
         };
       };
