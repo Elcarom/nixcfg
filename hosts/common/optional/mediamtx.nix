eb6f8166a499eebeae51d4b0b9b3cf6352486ffe
Elcarom <elcarom@proton.me>
2025-10-25 21:56:48 +0200

    Add MediaMTX service configuration for video processing

--- hosts/common/optional/mediamtx.nix
+++ hosts/common/optional/mediamtx.nix
@@ -15,4 +15,22 @@
     SUBSYSTEM=="video4linux", ATTR{name}=="cam_main", SYMLINK+="v4l/by-id/video-cam_main"
     SUBSYSTEM=="video4linux", ATTR{name}=="cam_sub",  SYMLINK+="v4l/by-id/video-cam_sub"
   '';
+
+  services.mediamtx = {
+    enable = true;
+    allowVideoAccess = true;
+
+    settings = {
+      paths = {
+        all_others = {
+          runOnInitRestart = true;
+        };
+        cam = {
+          runOnInit = ''
+            ${lib.getExe pkgs.ffmpeg-full} -y -hwaccel cuda -hwaccel_output_format cuda -f v4l2 -input_format mjpeg -video_size 1920x1080 -framerate 30 -i /dev/video0 -vf "scale_cuda=1920:1080" -c:v h264_nvenc -b:v 5M -f v4l2 /dev/v4l/by-id/video-cam_main &
+            ${lib.getExe pkgs.ffmpeg-full} -y -hwaccel cuda -hwaccel_output_format cuda -f v4l2 -input_format mjpeg -video_size 1920x1080 -framerate 30 -i /dev/video0 -vf "scale_cuda=640:360" -c:v h264_nvenc -b:v 1M -f v4l2 /dev/v4l/by-id/video-cam_sub  &
+         };
+      };
+   };
+};
 }
