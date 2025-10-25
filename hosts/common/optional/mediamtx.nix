e9ad8fdcc64a3170caffb25300d737a9a8de9652
Elcarom <elcarom@proton.me>
2025-10-25 13:40:05 +0200

    Refactor mediamtx configuration for improved clarity

--- hosts/common/optional/mediamtx.nix
+++ hosts/common/optional/mediamtx.nix
@@ -1,19 +1,26 @@
-{pkgs, lib, ...}: { 
-    config = {
-    services = {
-      mediamtx = {
-        allowVideoAccess = true;
-        enable = true;
-        settings = {
-          paths = {
-            cam = {
-              runOnInit = "${lib.getExe pkgs.ffmpeg} -y -hwaccel cuda -hwaccel_output_format cuda -f v4l2 -input_format mjpeg -video_size 1920x1080 -framerate 30 -i /dev/video0 -vf scale_cuda=1920:1080 -c:v h264_nvenc -b:v 5M -f rtsp rtsp://localhost:$RTSP_PORT/main -vf scale_cuda=640:360 -c:v h264_nvenc -b:v 8M -f rtsp rtsp://localhost:$RTSP_PORT/sub";
-              runOnInitRestart = true;
-            };
-            all_others = {
-              runOnInitRestart = true;
-            };
-          };
+{ pkgs, lib, ... }:
+{
+  environment.systemPackages = [
+    pkgs.ffmpeg-full
+  ];
+
+  services.mediamtx = {
+    enable = true;
+    allowVideoAccess = true;
+
+    settings = {
+      paths = {
+        cam = {
+          runOnInit = ''
+            ${lib.getExe pkgs.ffmpeg-full} -y \
+              -hwaccel cuda -hwaccel_output_format cuda \
+              -f v4l2 -input_format mjpeg -video_size 1920x1080 -framerate 30 \
+              -i /dev/video0 \
+              -filter_complex "[0:v]split=2[v1][v2];[v1]scale_cuda=1920:1080[vout1];[v2]scale_cuda=640:360[vout2]" \
+              -map "[vout1]" -c:v h264_nvenc -b:v 5M -f rtsp rtsp://localhost:8554/main \
+              -map "[vout2]" -c:v h264_nvenc -b:v 2M -f rtsp rtsp://localhost:8554/sub
+          '';
+          runOnInitRestart = true;
         };
       };
     };
