5bcc33019432e122c6edd45b77dd091fbb5c6cd8
Elcarom <elcarom@proton.me>
2025-10-25 21:57:38 +0200

    Refactor ffmpeg command for better readability

--- hosts/common/optional/mediamtx.nix
+++ hosts/common/optional/mediamtx.nix
@@ -1,7 +1,7 @@
 { pkgs, lib, config, ... }:
 
 {
-   environment.systemPackages = [
+  environment.systemPackages = [
     pkgs.mediamtx
     pkgs.ffmpeg-full
     pkgs.v4l-utils
@@ -9,7 +9,9 @@
 
   boot.kernelModules = [ "v4l2loopback" ];
   boot.extraModulePackages = [ pkgs.linuxPackages_latest.v4l2loopback ];
-  boot.extraModprobeConfig = '' options v4l2loopback devices=2 video_nr=1,2 card_label=cam_main,cam_sub exclusive_caps=1 '';
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=2 video_nr=1,2 card_label=cam_main,cam_sub exclusive_caps=1
+  '';
 
   services.udev.extraRules = ''
     SUBSYSTEM=="video4linux", ATTR{name}=="cam_main", SYMLINK+="v4l/by-id/video-cam_main"
@@ -27,10 +29,22 @@
         };
         cam = {
           runOnInit = ''
-            ${lib.getExe pkgs.ffmpeg-full} -y -hwaccel cuda -hwaccel_output_format cuda -f v4l2 -input_format mjpeg -video_size 1920x1080 -framerate 30 -i /dev/video0 -vf "scale_cuda=1920:1080" -c:v h264_nvenc -b:v 5M -f v4l2 /dev/v4l/by-id/video-cam_main &
-            ${lib.getExe pkgs.ffmpeg-full} -y -hwaccel cuda -hwaccel_output_format cuda -f v4l2 -input_format mjpeg -video_size 1920x1080 -framerate 30 -i /dev/video0 -vf "scale_cuda=640:360" -c:v h264_nvenc -b:v 1M -f v4l2 /dev/v4l/by-id/video-cam_sub  &
-         };
+            ${lib.getExe pkgs.ffmpeg-full} -y \
+              -hwaccel cuda -hwaccel_output_format cuda \
+              -f v4l2 -input_format mjpeg -video_size 1920x1080 -framerate 30 -i /dev/video0 \
+              -vf "scale_cuda=1920:1080" \
+              -c:v h264_nvenc -b:v 5M \
+              -f v4l2 /dev/v4l/by-id/video-cam_main &
+
+            ${lib.getExe pkgs.ffmpeg-full} -y \
+              -hwaccel cuda -hwaccel_output_format cuda \
+              -f v4l2 -input_format mjpeg -video_size 1920x1080 -framerate 30 -i /dev/video0 \
+              -vf "scale_cuda=640:360" \
+              -c:v h264_nvenc -b:v 1M \
+              -f v4l2 /dev/v4l/by-id/video-cam_sub &
+          '';
+        };
       };
-   };
-};
+    };
+  };
 }
