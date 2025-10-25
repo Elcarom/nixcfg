aba44ab80aa37de85e40001e32081a7e08cb413e
Elcarom <elcarom@proton.me>
2025-10-25 22:28:54 +0200

    Optimize cam runOnInit for hardware acceleration

--- hosts/common/optional/mediamtx.nix
+++ hosts/common/optional/mediamtx.nix
@@ -15,7 +15,7 @@
           runOnInitRestart = true;
         };
         cam = {
-          runOnInit = "${lib.getExe pkgs.ffmpeg-full} -y -f v4l2 -input_format mjpeg -video_size 1920x1080 -framerate 30 -i /dev/video0 -c:v libx264 -b:v 5M -f rtsp rtsp://localhost:$RTSP_PORT/main -vf scale=640:360 -c:v libx264 -b:v 1M -f rtsp rtsp://localhost:$RTSP_PORT/sub";
+          runOnInit = "${lib.getExe pkgs.ffmpeg-full} -y -hwaccel cuda -hwaccel_output_format cuda -f v4l2 -input_format mjpeg -video_size 1920x1080 -framerate 30 -i /dev/video0 -vf scale_cuda=1920:1080 -c:v h264_nvenc -b:v 5M -f rtsp rtsp://localhost:$RTSP_PORT/main -vf scale_cuda=640:360 -c:v h264_nvenc -b:v 1M -f rtsp rtsp://localhost:$RTSP_PORT/sub";
         };
       };
     };
